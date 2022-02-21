aff8143666f8eac1601ab57cc8397e9ff4cb0bd0
pstepien-design <pstepien@interia.pl>
2022-02-21 22:29:51 +0100

    adding homework exercise

---
+++ 3_World_Wide_Express/time/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname +'/public/time.html');
+});
+app.listen(8080, () => {
+  console.log('The server is running', 8080);
+});
