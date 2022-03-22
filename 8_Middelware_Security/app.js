817201f7e2c6df7fba888f904092e52f64407c51
pstepien-design <pstepien@interia.pl>
2022-03-22 15:12:59 +0100

    added routers and session to folder 8

--- 8_Middelware_Security/app.js
+++ 8_Middelware_Security/app.js
@@ -1,17 +1,52 @@
 import express from 'express';
 
 import helmet from 'helmet';
- 
-const app = express();
 
+import rateLimit from 'express-rate-limit';
+
+import session from 'express-session';
+
+import router from './routers/planets.js';
+
+const app = express();
+const baseLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use('/auth', authLimiter);
+app.use(baseLimiter);
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+  );
+  
+  app.use(router);
 app.use(express.static('public'));
 app.use('/auth/*', ipLogger);
 app.use(helmet());
 
 /* app.get("/clothes", (req, res) =>{
-res.sendFile(__dirname + "/public/clothes.html")
+  res.sendFile(__dirname + "/public/clothes.html")
 }); */
 
+app.post('/auth/login', (res, req) => {
+  res.send({ message: 'You are trying to log in...' });
+});
 app.get('/room', (req, res, next) => {
   console.log({ data: 'You are in room 1' });
   next();
