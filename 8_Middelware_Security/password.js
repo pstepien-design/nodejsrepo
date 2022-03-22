0f4b6e1aa7b6b5f504fef6921bcf6467d7900e39
pstepien-design <pstepien@interia.pl>
2022-03-22 15:46:54 +0100

    added login funcion in the password.js

--- 8_Middelware_Security/password.js
+++ 8_Middelware_Security/password.js
@@ -1,13 +1,18 @@
 import bcrypt from 'bcrypt';
-const saltRounds= 12;
-const plaintextPassowrd  = "hunter12";
+const saltRounds = 12;
+const plaintextPassowrd = 'hunter12';
+const hashedPassword =
+  '$2b$12$kegj1eIF8yUOrLSKaiaW/emFt5BauLJMGh0TOPChj9hgMgVdLwqNi';
 
-async function loginRouter(){
+console.log(bcrypt);
+async function loginRouter() {
+  const isSame = await bcrypt.compare(plaintextPassowrd, hashedPassword);
+  console.log(isSame);
 }
 
-async function singnupRouter(){
-const hashedPassword = await bcrypt.hash(plaintextPassowrd, saltRounds);
-console.log(hashedPassword);
+async function singnupRouter() {
+  const hashedPassword = await bcrypt.hash(plaintextPassowrd, saltRounds);
+  console.log(hashedPassword);
 }
 singnupRouter();
-console.log(bcrypt);
+loginRouter();
